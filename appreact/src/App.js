f421e295099291d38cec5f0f12c052d196ef9537
Juan Martin Amadi Dattero <jjmamadi@gmail.com>
2022-03-07 21:39:18 -0300

    modificacion

--- appreact/src/App.js
+++ appreact/src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
+import Cart from './components/Cart/Cart'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { CartContextProvider } from './context/CartContext';
 
-function App() {
+
+// import ItemCart from './components/ItemCart/ItemCart'
+
+
+const App = () => {
+
   return (
+    <body className='BodyFormat'>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CartContextProvider>
+        <BrowserRouter>
+          <NavBar />
+          <Routes>
+            <Route path='/' element={<ItemListContainer />}/>
+            <Route path='/about' element={<h1>About</h1>} />
+            <Route path='/category/:categoryId' element={<ItemListContainer />}/>
+            <Route path='/detail/:productId' element={<ItemDetailContainer />}/>
+            <Route path='/cart/' element={<Cart />}/>
+          </Routes>
+        </BrowserRouter>
+      </CartContextProvider>
     </div>
+    </body>
   );
 }
 
